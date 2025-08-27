7a6cb6241f596890a3cdd4b7b604387f8fa2ef20
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-28 01:29:33 +0530

    add custom css colors

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       
-      <Text className="text-5xl text-blue-500">Welcome</Text>
+      <Text className="text-6xl font-bold text-accent">Welcome</Text>
     </View>
   );
 }
