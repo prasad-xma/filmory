98a26d127323840aea2003328f1356ad26231428
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-29 01:38:30 +0530

    create tabs

---
+++ app/(tabs)/save.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Save = () => {
+  return (
+    <View>
+      <Text>Save</Text>
+    </View>
+  )
+}
+
+export default Save
+
+const styles = StyleSheet.create({})
