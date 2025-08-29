ec5f592c68c132224dbb83816748272bde88107d
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-08-29 21:14:07 +0530

    create tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import { Tabs } from 'expo-router'
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen
+            name='index'
+            options={{
+                title: 'Home',
+                headerShown: false
+            }}
+        />
+        <Tabs.Screen
+            name='save'
+            options={{
+                title: 'Save',
+                headerShown: false
+            }}
+        />
+        <Tabs.Screen
+            name='search'
+            options={{
+                title: 'search',
+                headerShown: false
+            }}
+        />
+         <Tabs.Screen
+            name='profile'
+            options={{
+                title: 'profile',
+                headerShown: false
+            }}
+        />
+    </Tabs>
+  )
+}
+
+export default _layout
